d6e56cbc68c11ab60c1d4f3ccaccdf28f1e609e5
Tanya Samastroyenka <tanyasamastr@gmail.com>
2022-05-26 16:04:05 +0300

    "Sign in" button

--- Netflix/Screens/Onboarding/OnboardingViewController.swift
+++ Netflix/Screens/Onboarding/OnboardingViewController.swift
@@ -9,15 +9,29 @@ import UIKit
 import SnapKit
 
 class OnboardingViewController: UIViewController {
+    
+    private let signInButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("SIGN IN", for: .normal)
+        button.backgroundColor = Asset.Colors.onboardingButtons.color
+        button.setTitleColor(.white, for: .normal)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let backgroundImageView = UIImageView(image: UIImage(named: Asset.Assets.onboarding.name))
         view.addSubview(backgroundImageView)
-        backgroundImageView.backgroundColor = .orange
+        view.addSubview(signInButton)
+        backgroundImageView.alpha = 0.5
         backgroundImageView.snp.makeConstraints { make in
             make.edges.equalTo(self.view)
+        }
+        signInButton.snp.makeConstraints { make in
+            make.width.equalToSuperview()
+            make.height.equalToSuperview().multipliedBy(0.05)
+            make.bottom.equalToSuperview().offset(-60)
         }
     }
 }
