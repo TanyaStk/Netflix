5f585b293bd0d92c9b2188eb3b015fef5199e54a
Tanya Samastroyenka <tanyasamastr@gmail.com>
2022-06-01 15:40:25 +0300

    add label to collectionView and gradient on latestFilm

--- Netflix/Screens/Dashboard/Home/HomeCollectionViewCell.swift
+++ Netflix/Screens/Dashboard/Home/HomeCollectionViewCell.swift
@@ -8,20 +8,20 @@
 import UIKit
 import SnapKit
 
-class HomeCollectionViewCell: UITableViewCell {
+class HomeCollectionViewCell: UICollectionViewCell {
     
     static let identifier = "HomeTableViewCell"
     
     let filmCoverImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(named: Asset.Assets.filmCover.name)
-        imageView.contentMode = .scaleToFill
         imageView.layer.cornerRadius = 8
+        imageView.clipsToBounds = true
         return imageView
     }()
     
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    override init(frame: CGRect) {
+        super.init(frame: frame)
         addSubview(filmCoverImageView)
         setConstraints()
     }
@@ -29,24 +29,10 @@ class HomeCollectionViewCell: UITableViewCell {
     required init?(coder: NSCoder) {
         fatalError()
     }
-        
+    
     private func setConstraints() {
         filmCoverImageView.snp.makeConstraints { make in
-            make.top.bottom.equalToSuperview()
-            make.width.equalToSuperview().multipliedBy(0.4)
+            make.edges.equalToSuperview()
         }
-    }
-}
-
-extension HomeCollectionViewCell: UICollectionViewDelegate, UICollectionViewDataSource {
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
-        cell.backgroundColor = .green
-        return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 20
     }
 }
--- Netflix/Screens/Dashboard/Home/HomeViewController.swift
+++ Netflix/Screens/Dashboard/Home/HomeViewController.swift
@@ -34,34 +34,43 @@ class HomeViewController: UIViewController {
         return button
     }()
     
-    private let feedCollection: UICollectionView = {
+    private let popularMoviesLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Popular Movies"
+        label.backgroundColor = .clear
+        label.textAlignment = .left
+        label.textColor = .white
+        label.font = UIFont.preferredFont(forTextStyle: .title3)
+        label.adjustsFontForContentSizeCategory = true
+        return label
+    }()
+    
+    private let popularMoviesCollection: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.register(HomeCollectionViewCell.self, forCellWithReuseIdentifier: HomeCollectionViewCell.identifier)
         return collectionView
     }()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        feedCollection.delegate = self
-        feedCollection.dataSource = self
+        popularMoviesCollection.delegate = self
+        popularMoviesCollection.dataSource = self
         addSubviews()
         setConstraints()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        addGradient()
     }
     
     private func addSubviews() {
         view.addSubview(latestFilm)
         view.addSubview(likeButton)
         view.addSubview(playButton)
-        view.addSubview(feedCollection)
-    }
-    
-    private func addNavigationBar() {
-        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(
-            named: Asset.Assets.logoNetflix.name), style: .done, target: self, action: nil)
-        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(
-            systemName: "person"), style: .done, target: self, action: nil)
+        view.addSubview(popularMoviesCollection)
+        popularMoviesCollection.addSubview(popularMoviesLabel)
     }
     
     private func setConstraints() {
@@ -81,51 +90,48 @@ class HomeViewController: UIViewController {
             make.centerY.equalTo(latestFilm.snp.bottom)
             make.centerX.equalToSuperview()
         }
-        feedCollection.snp.makeConstraints { make in
-            make.top.equalTo(playButton.snp.bottom).offset(16)
-            make.bottom.equalToSuperview()
+        popularMoviesLabel.snp.makeConstraints { make in
+            make.width.equalToSuperview()
+            make.top.equalToSuperview().offset(8)
+        }
+        popularMoviesCollection.snp.makeConstraints { make in
+            make.top.equalTo(playButton.snp.bottom).offset(8)
+            make.bottom.equalTo(self.view.safeAreaLayoutGuide.snp.bottom).offset(24)
             make.width.equalToSuperview()
         }
+    }
+    
+    private func addGradient() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = latestFilm.frame
+        gradientLayer.colors = [
+            UIColor.black.withAlphaComponent(0.5).cgColor,
+            UIColor.clear.cgColor,
+            UIColor.clear.cgColor,
+            UIColor.black.withAlphaComponent(0.5).cgColor
+        ]
+        latestFilm.layer.addSublayer(gradientLayer)
     }
 }
 
 extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 5
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HomeCollectionViewCell.identifier, for:
-                                                        indexPath) as? HomeCollectionViewCell else {
+                                                                indexPath) as? HomeCollectionViewCell else {
             return UICollectionViewCell()
         }
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-           let width = CGFloat(200)
-           let height = CGFloat(250)
-           
-           return CGSize(width: width, height: height)
-       }
-
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 16
+        let width = CGFloat(150)
+        let height = CGFloat(200)
+        
+        return CGSize(width: width, height: height)
     }
 }
-
-//    private let netflixButton: UIButton = {
-//        let button = UIButton()
-//        button.setImage(UIImage(named: Asset.Assets.logoNetflix.name), for: .normal)
-//        return button
-//    }()
-
-//    private let profileButton: UIButton = {
-//        let button = UIButton()
-//        button.setImage(UIImage(systemName: "person"), for: .normal)
-//        button.imageView?.contentMode = .scaleAspectFit
-//        button.tintColor = .white
-//        return button
-//    }()
-//
