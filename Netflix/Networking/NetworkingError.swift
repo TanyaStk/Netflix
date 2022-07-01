2370a30c401ebb5b1d254e5aa878a5f83d450e3c
Tanya Samastroyenka <tanyasamastr@gmail.com>
2022-07-01 16:19:49 +0300

    Handling errors

--- Netflix/Networking/NetworkingError.swift
+++ Netflix/Networking/NetworkingError.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+enum NetworkingError: Error {
+    
+    case invalidAPIKey
+    case resourceCouldNotBeFound
+    case unknown
+    case with(message: String)
+    
+    init(message: String) {
+        switch message {
+        case "Invalid API key: You must be granted a valid key.":
+            self = .invalidAPIKey
+        case "The resource you requested could not be found.":
+            self = .resourceCouldNotBeFound
+        default: self = message.isEmpty ? .unknown: .with(message: message)
+        }
+    }
+}
