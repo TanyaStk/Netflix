893b8e5e15998cd73fdaaf9e73db8a8d5d0f27ec
Tanya Samastroyenka <tanyasamastr@gmail.com>
2022-07-05 12:36:58 +0300

    improve keychain

--- Netflix/UseCases/KeychainUseCase.swift
+++ Netflix/UseCases/KeychainUseCase.swift
@@ -9,42 +9,76 @@ import Foundation
 
 class KeychainUseCase {
     
-    enum KeyChainError: Error {
+    enum KeychainError: Error {
         case duplicateEntry
+        case itemNotFound
+        case invalidItemFormat
         case unknown(OSStatus)
     }
     
-    static func save(account: String, password: String) throws {
+    private static let service = "Netflix"
+    
+    static func save(user: User) throws {
+        let password = user.password.data(using: .utf8) ?? Data()
+        
         let query: [String: AnyObject] = [
             kSecClass as String: kSecClassGenericPassword,
-            kSecAttrAccount as String: account as AnyObject,
-            kSecValueData as String: password.data(using: .utf8) as AnyObject
+            kSecAttrService as String: service as AnyObject,
+            kSecAttrAccount as String: user.login as AnyObject,
+            kSecValueData as String: password as AnyObject
         ]
         
         let status = SecItemAdd(query as CFDictionary, nil)
         
         guard status != errSecDuplicateItem else {
-            throw KeyChainError.duplicateEntry
+            throw KeychainError.duplicateEntry
         }
         
         guard status == errSecSuccess else {
-            throw KeyChainError.unknown(status)
+            throw KeychainError.unknown(status)
         }
-        
-        print("saved")
     }
     
-    static func get(account: String) -> Data? {
+    static func getUser() throws -> User? {
         let query: [String: AnyObject] = [
             kSecClass as String: kSecClassGenericPassword,
-            kSecAttrAccount as String: account as AnyObject
+            kSecAttrService as String: service as AnyObject,
+            kSecReturnAttributes as String: kCFBooleanTrue,
+            kSecReturnData as String: kCFBooleanTrue
         ]
         
         var result: AnyObject?
         let status = SecItemCopyMatching(query as CFDictionary, &result)
         
-        print("Read status: \(status)")
+        guard status != errSecItemNotFound else {
+            throw KeychainError.itemNotFound
+        }
         
-        return result as? Data
+        guard status == errSecSuccess else {
+            throw KeychainError.unknown(status)
+        }
+        
+        guard let dict = result as? [String: AnyObject],
+              let passwordData = dict[String(kSecValueData)] as? Data,
+              let password = String(data: passwordData, encoding: .utf8),
+              let login = (dict[String(kSecAttrAccount)] as? String) else {
+            throw KeychainError.invalidItemFormat
+        }
+        
+        return User(login: login, password: password)
+    }
+    
+    static func deletePassword(for user: User) throws {
+        let query: [String: AnyObject] = [
+            kSecAttrService as String: service as AnyObject,
+            kSecAttrAccount as String: user.login as AnyObject,
+            kSecClass as String: kSecClassGenericPassword
+        ]
+        
+        let status = SecItemDelete(query as CFDictionary)
+        
+        guard status == errSecSuccess else {
+            throw KeychainError.unknown(status)
+        }
     }
 }
