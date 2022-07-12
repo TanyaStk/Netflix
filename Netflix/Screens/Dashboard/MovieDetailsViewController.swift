97e057214797e13e1c9805e32bac6c54040e33c5
Tanya Samastroyenka <tanyasamastr@gmail.com>
2022-07-13 01:02:41 +0300

    Group elements into stackViews

--- Netflix/Screens/Dashboard/MovieDetailsViewController.swift
+++ Netflix/Screens/Dashboard/MovieDetailsViewController.swift
@@ -44,6 +44,16 @@ class MovieDetailsViewController: UIViewController {
         return button
     }()
     
+    lazy var navigationButtonsStackView: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .horizontal
+        stack.alignment = .fill
+        stack.distribution = .equalSpacing
+        [self.backButton,
+         self.likeButton].forEach { stack.addArrangedSubview($0) }
+        return stack
+    }()
+    
     private let playButton: UIButton = {
         let button = UIButton()
         button.setImage(UIImage(systemName: "play.fill"), for: .normal)
@@ -74,6 +84,17 @@ class MovieDetailsViewController: UIViewController {
         return label
     }()
     
+    lazy var runtimeStackView: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .horizontal
+        stack.alignment = .fill
+        stack.spacing = -60
+        stack.distribution = .equalSpacing
+        [self.runtimeIcon,
+         self.runtimeLabel].forEach { stack.addArrangedSubview($0) }
+        return stack
+    }()
+    
     private let ratingIcon: UIImageView = {
         let imageView = UIImageView(image: UIImage(systemName: "star.fill"))
         imageView.tintColor = Asset.Colors.loginTexts.color
@@ -90,6 +111,17 @@ class MovieDetailsViewController: UIViewController {
         label.adjustsFontForContentSizeCategory = true
         label.numberOfLines = 0
         return label
+    }()
+    
+    lazy var ratingStackView: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .horizontal
+        stack.alignment = .fill
+        stack.spacing = -50
+        stack.distribution = .equalSpacing
+        [self.ratingIcon,
+         self.ratingLabel].forEach { stack.addArrangedSubview($0) }
+        return stack
     }()
     
     private let releaseDateHeaderLabel: UILabel = {
@@ -114,6 +146,16 @@ class MovieDetailsViewController: UIViewController {
         label.adjustsFontForContentSizeCategory = true
         label.numberOfLines = 0
         return label
+    }()
+    
+    lazy var releaseDateStackView: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.alignment = .leading
+        stack.distribution = .fillProportionally
+        [self.releaseDateHeaderLabel,
+         self.releaseDateLabel].forEach { stack.addArrangedSubview($0) }
+        return stack
     }()
     
     private let synopsisHeaderLabel: UILabel = {
@@ -142,6 +184,16 @@ class MovieDetailsViewController: UIViewController {
         return label
     }()
     
+    lazy var synopsisStackView: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.alignment = .leading
+        stack.distribution = .fillProportionally
+        [self.synopsisHeaderLabel,
+         self.synopsisDescriptionLabel].forEach { stack.addArrangedSubview($0) }
+        return stack
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         addSubviews()
@@ -151,17 +203,12 @@ class MovieDetailsViewController: UIViewController {
     private func addSubviews() {
         view.addSubview(movieCoverImageView)
         view.addSubview(movieTitleLabel)
-        view.addSubview(backButton)
-        view.addSubview(likeButton)
+        view.addSubview(navigationButtonsStackView)
         view.addSubview(playButton)
-        view.addSubview(runtimeIcon)
-        view.addSubview(runtimeLabel)
-        view.addSubview(ratingIcon)
-        view.addSubview(ratingLabel)
-        view.addSubview(releaseDateHeaderLabel)
-        view.addSubview(releaseDateLabel)
-        view.addSubview(synopsisHeaderLabel)
-        view.addSubview(synopsisDescriptionLabel)
+        view.addSubview(runtimeStackView)
+        view.addSubview(ratingStackView)
+        view.addSubview(releaseDateStackView)
+        view.addSubview(synopsisStackView)
     }
     
     private func setConstraints() {
@@ -171,18 +218,10 @@ class MovieDetailsViewController: UIViewController {
             make.height.equalToSuperview().multipliedBy(0.4)
         }
         
-        backButton.snp.makeConstraints { make in
-            make.height.equalToSuperview().multipliedBy(0.05)
-            make.width.equalTo(backButton.snp.height)
+        navigationButtonsStackView.snp.makeConstraints { make in
+            make.width.equalToSuperview().multipliedBy(0.9)
             make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(-8)
-            make.left.equalToSuperview().offset(8)
-        }
-        
-        likeButton.snp.makeConstraints { make in
-            make.height.equalToSuperview().multipliedBy(0.05)
-            make.width.equalTo(likeButton.snp.height)
-            make.centerY.equalTo(backButton.snp.centerY)
-            make.right.equalToSuperview().offset(-8)
+            make.centerX.equalToSuperview()
         }
         
         playButton.snp.makeConstraints { make in
@@ -198,54 +237,27 @@ class MovieDetailsViewController: UIViewController {
             make.top.equalTo(playButton.snp.bottom).offset(8)
         }
         
-        runtimeIcon.snp.makeConstraints { make in
-            make.width.equalToSuperview().multipliedBy(0.05)
-            make.height.equalTo(ratingIcon.snp.width)
+        runtimeStackView.snp.makeConstraints { make in
+            make.height.equalToSuperview().multipliedBy(0.02)
             make.left.equalToSuperview().offset(8)
             make.top.equalTo(movieTitleLabel.snp.bottom)
         }
         
-        runtimeLabel.snp.makeConstraints { make in
-            make.height.equalTo(runtimeIcon.snp.height)
-            make.width.equalToSuperview().multipliedBy(0.45)
-            make.left.equalTo(runtimeIcon.snp.right).offset(8)
-            make.centerY.equalTo(runtimeIcon.snp.centerY)
+        ratingStackView.snp.makeConstraints { make in
+            make.height.equalToSuperview().multipliedBy(0.02)
+            make.left.equalTo(runtimeStackView.snp.right).offset(8)
+            make.top.equalTo(movieTitleLabel.snp.bottom)
         }
-        
-        ratingIcon.snp.makeConstraints { make in
-            make.width.equalToSuperview().multipliedBy(0.05)
-            make.height.equalTo(ratingIcon.snp.width)
-            make.left.equalTo(runtimeLabel.snp.right)
-            make.centerY.equalTo(runtimeIcon.snp.centerY)
-        }
-        
-        ratingLabel.snp.makeConstraints { make in
-            make.height.equalTo(ratingIcon.snp.height)
-            make.width.equalToSuperview().multipliedBy(0.45)
-            make.left.equalTo(ratingIcon.snp.right).offset(8)
-            make.centerY.equalTo(runtimeIcon.snp.centerY)
-        }
-        
-        releaseDateHeaderLabel.snp.makeConstraints { make in
+            
+        releaseDateStackView.snp.makeConstraints { make in
             make.width.equalToSuperview()
-            make.top.equalTo(ratingIcon.snp.bottom).offset(16)
+            make.top.equalTo(ratingStackView.snp.bottom).offset(16)
         }
         
-        releaseDateLabel.snp.makeConstraints { make in
+        synopsisStackView.snp.makeConstraints { make in
             make.width.equalToSuperview()
-            make.top.equalTo(releaseDateHeaderLabel.snp.bottom).offset(8)
+            make.top.equalTo(releaseDateStackView.snp.bottom).offset(16)
         }
-        
-        synopsisHeaderLabel.snp.makeConstraints { make in
-            make.width.equalToSuperview()
-            make.top.equalTo(releaseDateLabel.snp.bottom).offset(16)
-        }
-        
-        synopsisDescriptionLabel.snp.makeConstraints { make in
-            make.width.equalToSuperview()
-            make.top.equalTo(synopsisHeaderLabel.snp.bottom).offset(8)
-        }
-        
     }
     
 }
