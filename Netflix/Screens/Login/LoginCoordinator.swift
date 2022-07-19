658b782c8e68bb923e850407608b0f0d47228f17
Tanya Samastroyenka <tanyasamastr@gmail.com>
2022-07-19 16:02:14 +0300

    Login coordinator

--- Netflix/Screens/Login/LoginCoordinator.swift
+++ Netflix/Screens/Login/LoginCoordinator.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import UIKit
+
+class LoginCoordinator: Coordinator {
+    
+    private let navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+
+    func start() {
+        let loginViewController = LoginViewController()
+        let loginViewModel = LoginViewModel(loginService: UserInfoAPI(), keychainUseCase: KeychainUseCase())
+        loginViewController.viewModel = loginViewModel
+        loginViewController.coordinator = self
+        
+        navigationController.pushViewController(loginViewController, animated: false)
+    }
+    
+    func coordinateToDashboard() {
+        let dashboardCoordinator = DashboardCoordinator(navigationController: navigationController)
+        dashboardCoordinator.start()
+    }
+}
