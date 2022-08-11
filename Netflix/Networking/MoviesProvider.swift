d7635b3142990a976486dda9ddc2654a0bdc89e9
Tanya Samastroyenka <tanyasamastr@gmail.com>
2022-08-11 15:45:53 +0300

    Movies provider

--- Netflix/Networking/MoviesProvider.swift
+++ Netflix/Networking/MoviesProvider.swift
@@ -11,11 +11,10 @@ import Moya
 
 protocol Movies {
     func getLatest() -> Single<GetLatestResponse>
+    func getUpcoming(page: Int) -> Single<MoviesResultsResponse>
     func getPopular(page: Int) -> Single<MoviesResultsResponse>
-    func getUpcoming() -> Single<MoviesResultsResponse>
-    func getDetails(for movieId: Int) -> Single<MovieDetailsResponse>
-    func search(for movie: String) -> Single<MoviesResultsResponse>
-    func getVideos(for movieId: Int) -> Single<GetVideosResponse>
+    func getDetails(movieId: String) -> Single<MovieDetailsResponse>
+    func search(for movie: String, on page: Int) -> Single<MoviesResultsResponse>
 }
 
 final class MoviesProvider: Movies {
@@ -34,27 +33,21 @@ final class MoviesProvider: Movies {
             .map(MoviesResultsResponse.self)
     }
     
-    func getUpcoming() -> Single<MoviesResultsResponse> {
-        return provider.rx.request(.upcoming)
+    func getUpcoming(page: Int) -> Single<MoviesResultsResponse> {
+        return provider.rx.request(.upcoming(page: page))
             .catchResponseError(NetworkingErrorResponse.self)
             .map(MoviesResultsResponse.self)
     }
     
-    func getDetails(for movieId: Int) -> Single<MovieDetailsResponse> {
+    func getDetails(movieId: String) -> Single<MovieDetailsResponse> {
         return provider.rx.request(.details(movieId: movieId))
             .catchResponseError(NetworkingErrorResponse.self)
             .map(MovieDetailsResponse.self)
     }
     
-    func search(for movie: String) -> Single<MoviesResultsResponse> {
-        return provider.rx.request(.search(query: movie))
+    func search(for movie: String, on page: Int) -> Single<MoviesResultsResponse> {
+        return provider.rx.request(.search(query: movie, page: page))
             .catchResponseError(NetworkingErrorResponse.self)
             .map(MoviesResultsResponse.self)
-    }
-    
-    func getVideos(for movieId: Int) -> Single<GetVideosResponse> {
-        return provider.rx.request(.videos(movieId: movieId))
-            .catchResponseError(NetworkingErrorResponse.self)
-            .map(GetVideosResponse.self)
     }
 }
